b4f0e3caca9e21ab0d154452d2c360806d510c11
Chae-vin <arvin.santillan@cit.edu>
2023-11-20 13:56:46 +0800

    Updated Code

---
+++ src/EventList.js
@@ -0,0 +1,31 @@
+import Header from "../../BarangayConnect_Project/src/Header";
+import React from "react";
+
+export default function EventList() {
+  return (
+    <div
+      style={{
+        background: "#213555",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <div>
+        <Header />
+      </div>
+      <div
+        style={{
+          background: `url('buildingbg.png')`,
+          backgroundSize: "100% 100%",
+          backgroundRepeat: "no-repeat",
+          height: "100%",
+        }}
+      >
+        <h1 style={{ fontSize: "60px", color: "white", margin: "60px" }}>
+          EVENT LIST:
+        </h1>
+      </div>
+    </div>
+  );
+}
