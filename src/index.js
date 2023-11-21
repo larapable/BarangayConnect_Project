426912ae7a867f7dac83e02f6f18c09566651f7c
Chae-vin <arvin.santillan@cit.edu>
2023-11-21 16:32:53 +0800

    index ni lara

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Home from './Home';
+import Forum from './Forum';
+import Header from './Header';
+import Requests from './Requests';
+import reportWebVitals from './reportWebVitals';
+import PaymentRem from './PaymentRem';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     {/* <Forum/> */}
     {/* <Home/> */}
     {/* <Header/> */}
-    {/* <Requests/> */}
-    <BrowserRouter>
-      <Start />
-    </BrowserRouter>
+    <Requests/>
+    {/* <PaymentRem/> */}
+    
   </React.StrictMode>
 );
 
