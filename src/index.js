5e0e4eed3a1aa2160b94df9e97e6aef6a818c2a6
ArzielMae <arzielmae.lawas@cit.edu>
2023-11-27 00:19:15 +0800

    updated code

--- src/index.js
+++ src/index.js
@@ -11,14 +11,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
+
     <BrowserRouter>
-    {/* <Forum/> */}
-    {/* <Home/> */}
-    {/* <Header/> */}
-    <App/>
+      <App />
     </BrowserRouter>
-    {/* <PaymentRem/> */}
-    
+
+  
   </React.StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
